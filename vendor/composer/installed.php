ec72164a7f1c17ed44e0df400913d14ebec78ccc
qbac <jakub.cyrklaf@gmail.com>
2022-06-13 12:26:00 +0200

    add official woocommerce package

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e3bfd74984d526ac364c756da852f5f17eb6c2f0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e3bfd74984d526ac364c756da852f5f17eb6c2f0',
+            'dev_requirement' => false,
+        ),
+        'automattic/woocommerce' => array(
+            'pretty_version' => '3.1.0',
+            'version' => '3.1.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../automattic/woocommerce',
+            'aliases' => array(),
+            'reference' => 'd3b292f04c0b3b21dced691ebad8be073a83b4ad',
+            'dev_requirement' => false,
+        ),
+    ),
+);
